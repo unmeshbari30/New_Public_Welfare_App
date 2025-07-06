8d90f6de457330709f2c859c6b3aa5ba970ebd26
unmeshbari <unmesh.bari@rheal.com>
2025-07-06 20:22:01 +0530

    color changed.

--- lib/screen/Login_Screens/sign_up_screen.dart
+++ lib/screen/Login_Screens/sign_up_screen.dart
@@ -32,222 +32,11 @@ class _SignUpScreenState extends ConsumerState<SignUpScreen> {
 
   var formKey = GlobalKey<FormState>();
 
-  // Widget getScaffold(AuthenticationState state) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: Text("Register",
-  //           style: TextStyle(color: Theme.of(context).secondaryHeaderColor)),
-  //       centerTitle: true,
-  //       backgroundColor: Colors.black,
-  //       iconTheme: IconThemeData(color: Colors.white),
-  //     ),
-  //     backgroundColor: Colors.black,
-  //     body: SafeArea(
-  //       child: SingleChildScrollView(
-  //         child: Form(
-  //           autovalidateMode: AutovalidateMode.onUserInteraction,
-  //           key: formKey,
-  //           child: Column(
-  //             children: [
-  //               SizedBox(
-  //                 height: 10,
-  //               ),
-  //               Padding(
-  //                 padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-  //                 child: CustomFilledTextField(
-  //                   controller: state.firstNameController,
-  //                   labelText: "पहिले नाव / First Name *",
-  //                   validator: (value) {
-  //                   return Validators.validateEmptyField(value);
-  //                   }
-  //                 ),
-  //               ),
-  //               SizedBox(
-  //                 height: 10,
-  //               ),
-  //               Padding(
-  //                 padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-  //                 child: CustomFilledTextField(
-  //                   controller: state.middleNameController,
-  //                   labelText: "मधले नाव / Middle Name",
-  //                 ),
-  //               ),
-  //               SizedBox(
-  //                 height: 10,
-  //               ),
-  //               Padding(
-  //                 padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-  //                 child: CustomFilledTextField(
-  //                   controller: state.lastNameController,
-  //                   labelText: "आडनाव / Last Name *",
-  //                   validator: (value) {
-  //                     return Validators.validateEmptyField(value);
-  //                   },
-  //                 ),
-  //               ),
-  //               Row(
-  //                 children: [
-  //                   Expanded(
-  //                     child: Padding(
-  //                       padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
-  //                       child: FutureFilledDropdown(
-  //                           items: state.gendersList,
-  //                           controller: state.gendersController,
-  //                           labelText: "लिंग / Gender *",
-  //                           titleBuilder: (item) => item,
-  //                           validator: (value) {
-  //                             return Validators.validateEmptyField(value);
-  //                           },
-  //                           ),
-  //                     ),
-  //                   ),
-  //                   Expanded(
-  //                     child: Padding(
-  //                       padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-  //                       child: FutureFilledDropdown(
-  //                         items: state.tehsilList,
-  //                         controller: state.tehsilController,
-  //                         labelText: "तालुका  / Taluka *",
-  //                         titleBuilder: (item) => item,
-  //                         validator: (value) {
-  //                           return Validators.validateEmptyField(value);
-  //                         },
-  //                       ),
-  //                     ),
-  //                   ),
-  //                 ],
-  //               ),
-  //               Row(
-  //                 children: [
-  //                   Expanded(
-  //                     child: Padding(
-  //                       padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
-  //                       child: CustomFilledTextField(
-  //                       controller: state.ageController,
-  //                       labelText: "वय / Age *",
-  //                       keyboardType: TextInputType.number,
-  //                       validator: (value) {
-  //                     return Validators.validateEmptyField(value);
-  //                   },
-  //                 ),
-  //                     ),
-  //                   ),
-  //                   Expanded(
-  //                     child: Padding(
-  //                       padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-  //                       child: FutureFilledDropdown(
-  //                         items: state.bloodGroup,
-  //                         controller: state.bloodGroupController,
-  //                         labelText: "रक्त गट / Blood Group *",
-  //                         titleBuilder: (item) => item,
-  //                         validator: (value) {
-  //                           return Validators.validateEmptyField(value);
-  //                         },
-  //                       ),
-  //                     ),
-  //                   ),
-  //                 ],
-  //               ),
-  //               Padding(
-  //                 padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-  //                 child: CustomFilledTextField(
-  //                   controller: state.emailController,
-  //                   labelText: "ईमेल / Email",
-  //                 ),
-  //               ),
-  //               Padding(
-  //                 padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-  //                 child: CustomFilledTextField(
-  //                   controller: state.whatsappNumberController,
-  //                   labelText: "व्हाट्सअँप नं. / Whatsapp No.",
-  //                   validator: (value) {
-  //                     return Validators.validateMobileNumber(value);
-  //                   },
-  //                 ),
-  //               ),
-  //               Padding(
-  //                 padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-  //                 child: CustomFilledTextField(
-  //                   controller: state.mobileNumberController,
-  //                   labelText: "मो. नंबर / Mobile Number*",
-  //                   validator: (value) {
-  //                     return Validators.validateMobileNumber(value);
-  //                   },
-  //                 ),
-  //               ),
-  //               Padding(
-  //                 padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-  //                 child: CustomFilledTextField(
-  //                   controller: state.passwordController,
-  //                   labelText: "पासवर्ड तयार करा / Create Password *",
-  //                   validator: (value) {
-  //                     return Validators.validateEmptyField(value);
-  //                   },
-  //                 ),
-  //               ),
-  //               Padding(
-  //                 padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-  //                 child: ElevatedButton(
-  //                     onPressed: () async{
-  //                       if (formKey.currentState!.validate()) {
-  //                         try{
-  //                           EasyLoading.show();
-  //                           var successful = await ref.read(authenticationControllerProvider.notifier).userRegistration();
-  //                           if(successful?.isRegistered == true){
-  //                             ScaffoldMessenger.of(context).showSnackBar(
-  //                               SnackBar(
-  //                                 content: Text(
-  //                                   successful?.message ?? "",
-  //                                   style: TextStyle(color: Colors.black),
-  //                                 ),
-  //                                 duration: Duration(seconds: 2),
-  //                                 backgroundColor: Colors.green.shade600,
-  //                               ),
-  //                             );
-  //                             Navigator.pop(context);
-  //                           }else{
-  //                             ScaffoldMessenger.of(context).showSnackBar(
-  //                               SnackBar(
-  //                                 content: Text(
-  //                                  successful?.message ?? "Something went wrong",
-  //                                   style: TextStyle(color: Colors.black),
-  //                                 ),
-  //                                 duration: Duration(seconds: 2),
-  //                                 backgroundColor: Colors.red.shade600,
-  //                               ),
-  //                             );
-  //                           }
-  //                         }catch(e){
-  //                            ScaffoldMessenger.of(context).showSnackBar(
-  //                               SnackBar(
-  //                                 content: Text(
-  //                                 "Something went wrong",
-  //                                   style: TextStyle(color: Colors.black),
-  //                                 ),
-  //                                 duration: Duration(seconds: 2),
-  //                                 backgroundColor: Colors.red.shade600,
-  //                               ),
-  //                             );
-  //                         }finally{
-  //                           EasyLoading.dismiss();
-  //                         }
-  //                       }
-  //                     },
-  //                     child: Text("रजिस्टर / Register")),
-  //               )
-  //             ],
-  //           ),
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
-
   Widget getScaffold(AuthenticationState state) {
     return Scaffold(
       backgroundColor: const Color(0xFFF1F5F9), // Light bluish background
       appBar: AppBar(
-        title: const Text("Register", style: TextStyle(color: Colors.white)),
+        title: const Text("Register", style: TextStyle(color: Colors.black)),
         centerTitle: true,
         backgroundColor: Colors.amber,
         iconTheme: const IconThemeData(color: Colors.white),
@@ -274,7 +63,7 @@ class _SignUpScreenState extends ConsumerState<SignUpScreen> {
                       style: TextStyle(
                         fontSize: 24,
                         fontWeight: FontWeight.bold,
-                        color: Colors.amber,
+                        color: Colors.black,
                       ),
                     ),
                     const SizedBox(height: 20),
@@ -451,7 +240,7 @@ class _SignUpScreenState extends ConsumerState<SignUpScreen> {
                           borderRadius: BorderRadius.circular(12),
                         ),
                       ),
-                      child: const Text("रजिस्टर / Register"),
+                      child: const Text("रजिस्टर / Register", style: TextStyle(color: Colors.black),),
                     ),
                   ],
                 ),
